3b4851c5a13f669800a0ae2a774a85b953cb2f7b
eric soucy <eric.soucy@gmail.com>
2022-03-08 20:21:44 -0500

    router in ath module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
+import { RouterModule } from '@angular/router';
 
-
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class AuthModule { }
+export class AuthModule {}
