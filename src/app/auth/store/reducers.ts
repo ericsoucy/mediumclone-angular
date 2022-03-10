83779206c570c2e0bebfb11a02f1c826946bcf50
eric soucy <eric.soucy@gmail.com>
2022-03-09 20:22:05 -0500

    create reducer and interfaces

---
+++ src/app/auth/store/reducers.ts
@@ -0,0 +1,22 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { AuthState } from '../types/auth-state';
+import { registerAction } from './actions';
+
+const initialState: AuthState = {
+  isSubmitting: false,
+};
+
+const authReducer = createReducer(
+  initialState,
+  on(
+    registerAction,
+    (state): AuthState => ({
+      ...state,
+      isSubmitting: true,
+    })
+  )
+);
+
+export function reducers(state: AuthState, action: Action) {
+  return authReducer(state, action);
+}
